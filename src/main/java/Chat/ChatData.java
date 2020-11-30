60a0fb747f4b34e4fa299f59bdb46b1b4964a003
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-11-30 13:40:46 -0600

    Replace int user ids with ChatUser

--- src/main/java/Chat/ChatData.java
+++ src/main/java/Chat/ChatData.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 public class ChatData implements Serializable {
     private static final long serialVersionUID = -2270421533467436142L;
 
-    public int myId;
-    public int friendId;
+    public ChatUser me;
+    public ChatUser friend;
 
     public String myMessage;
     public String friendMessage;
