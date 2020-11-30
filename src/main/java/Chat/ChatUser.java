a40d09a3041f52d454b6d0123d5496bd2f9668cd
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-11-30 13:39:57 -0600

    Add chat user

---
+++ src/main/java/Chat/ChatUser.java
@@ -0,0 +1,10 @@
+package Chat;
+
+import java.io.Serializable;
+
+public class ChatUser implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public String name;
+    public int id;
+}
