bb6bedf78534784f368d141c79d8303d82b60cb9
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-12-01 18:47:13 -0600

    Update ChatData

--- src/main/java/Chat/ChatData.java
+++ src/main/java/Chat/ChatData.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 public class ChatData implements Serializable {
     private static final long serialVersionUID = -2270421533467436142L;
 
-    public ChatUser me;
-    public ChatUser friend;
+    public ChatUser from;
+    public ChatUser to;
 
     public String message;
 }
