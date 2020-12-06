76925008c93311090f6e8c91f1696b416dc0028b
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-12-06 17:56:41 -0600

    List not Serializable

--- src/main/java/Chat/ChatData.java
+++ src/main/java/Chat/ChatData.java
@@ -1,15 +1,15 @@
 package Chat;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.ArrayList;
 
 public class ChatData implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
     public String message = null;
     public ChatUser from = null;
     public ChatUser to = null;
-    public List<ChatUser> clients = null;
+    public ArrayList<ChatUser> clients = null;
 
     public static class ChatUser implements Serializable {
         private static final long serialVersionUID = 1L;
